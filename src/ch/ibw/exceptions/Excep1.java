bfb9eb3ff5e5c25abdcd48344a5d812f897132d9
Michele <msferlazza@gmail.com>
2016-10-25 18:45:16 +0200

    Diverse Bsp. gemacht mit try/catch

--- src/ch/ibw/exceptions/Excep1.java
+++ src/ch/ibw/exceptions/Excep1.java
@@ -5,9 +5,15 @@ import Prog1Tools.IOTools;
 public class Excep1 {
 
   public static void main(String[] args) {
-    int a = IOTools.readInteger("a=");
-    int b = IOTools.readInteger("b=");
-    System.out.println("a/b="+(a/b));
+
+    try {
+      int a = IOTools.readInteger("a=");
+      int b = IOTools.readInteger("b=");
+      System.out.println("a/b=" + (a / b));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
--- src/ch/ibw/exceptions/LiesAusDatei.java
+++ src/ch/ibw/exceptions/LiesAusDatei.java
@@ -1,22 +1,35 @@
 package ch.ibw.exceptions;
 
-import java.io.*;
-import Prog1Tools.*;
+import Prog1Tools.IOTools;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LiesAusDatei {
-  public static void main(String[] args) {
-    // Lies den Dateinamen ein:
-    String dateiname = IOTools.readString("Dateiname: ");
-    // Oeffne die Datei zum Lesen:
-    FileReader dateileser = new FileReader(dateiname);
-    // Lies alle Zeichen aus der Datei ein (read liefert int)
-    // bis das Dateiende erreicht wird (signalisiert durch -1)
-    // und gib sie (wieder als Buchstabe) auf den Bildschirm aus:
-    System.out.println("In der Datei " + dateiname + " steht");
-    while (true) {
-      int gelesen = dateileser.read(); 
-      if (gelesen == -1)
-        break;
-      System.out.print((char) gelesen);
+  public static void main(String[] args)
+  {
+
+    try {
+      // Lies den Dateinamen ein:
+      String dateiname = IOTools.readString("Dateiname: ");
+      // Oeffne die Datei zum Lesen:
+      FileReader dateileser = new FileReader(dateiname);
+      // Lies alle Zeichen aus der Datei ein (read liefert int)
+      // bis das Dateiende erreicht wird (signalisiert durch -1)
+      // und gib sie (wieder als Buchstabe) auf den Bildschirm aus:
+      System.out.println("In der Datei " + dateiname + " steht");
+      while (true) {
+        int gelesen = dateileser.read();
+        if (gelesen == -1)
+          break;
+        System.out.print((char) gelesen);
+      }
+
+    } catch (FileNotFoundException e) {
+      System.out.println("Fehler: "+e.getMessage());
+    } catch(IOException e) {
+      System.out.println("Fehler: "+e.getMessage());
     }
   }
 }
